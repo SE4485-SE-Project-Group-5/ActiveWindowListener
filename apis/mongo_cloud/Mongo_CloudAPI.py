b775b3843d1064a7e8693c8caec69d0b1eb0af14
Taylor Benavides <taylorscottsoftware@gmail.com>
2020-10-22 22:12:13 -0500

    Updated mongo server

--- apis/mongo_cloud/Mongo_CloudAPI.py
+++ apis/mongo_cloud/Mongo_CloudAPI.py
@@ -3,8 +3,8 @@ import pymongo
 from pymongo import MongoClient
 from getmac import get_mac_address as gma
 
-client = MongoClient("mongodb+srv://Admin:Pass123word#@cluster0.vh3ty.mongodb.net/WindowListener?retryWrites=true&w=majority")
-db = client['WindowListener']
+client = MongoClient("mongodb+srv://admin:mongodb9143@cluster0.femb8.mongodb.net/group5db?retryWrites=true&w=majority")
+db = client['group5db']
 
 def insert_log():
     collection = db[gma()]
@@ -13,10 +13,10 @@ def insert_log():
 
     if isinstance(file_data, list):
         collection.insert_many(file_data)
-        print("IF statement")
+        print("Successfully inserted log as many files")
     else:
         collection.insert_one(file_data)
-        print("else statement")
+        print("Successfully inserted log as single file")
 
 def create_collection():
     collection = gma()
@@ -26,5 +26,7 @@ def create_collection():
     else:
         db.create_collection(collection)
         print("Collection: {} was created successfully!".format(collection))
+
+create_collection()
 
 client.close()
